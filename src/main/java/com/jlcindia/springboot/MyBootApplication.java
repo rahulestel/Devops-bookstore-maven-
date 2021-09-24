87d6d5a6dea89dbf28721d073e1ebbd25f80ab53
rahulestel <90347942+rahulestel@users.noreply.github.com>
2021-09-25 01:13:19 +0530

    Update MyBootApplication.java

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyBootApplication {
 public static void main(String[] args) {
 System.out.println("Starting Boot Application");
-System.out.println("Starting Boot Application");
+System.out.println("updated by Rahul");
 SpringApplication.run(MyBootApplication.class, args);
 }
 }
