c3d274a568f8c14429066d57977663fcbc3729d0
rahulestel <90347942+rahulestel@users.noreply.github.com>
2021-10-01 00:02:59 +0530

    Update MyBootApplication.java

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -8,6 +8,7 @@ public class MyBootApplication {
 public static void main(String[] args) {
 System.out.println("Starting Boot Application");
 System.out.println("updated by Rahul Rakesh");
+System.out.println("updated by Rahul Devops");  
 SpringApplication.run(MyBootApplication.class, args);
 }
 }
